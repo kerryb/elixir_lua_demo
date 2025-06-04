f0eee6a569254b8ad5ceaa7af1fca043bbd3c0f3
Kerry Buckley <kerryjbuckley@gmail.com>
2025-06-04 11:00:06 +0100

    Demo calling into Lua and back to Elixir

--- lib/demo.ex
+++ lib/demo.ex
@@ -1,2 +1,9 @@
 defmodule Demo do
+  use Lua.API, scope: "demo"
+
+  deflua(my_name, do: "an Elixir Lua demo")
+
+  def greet(script, name) do
+    Lua.new() |> Lua.load_api(__MODULE__) |> Lua.set!([:name], name) |> Lua.eval!(script)
+  end
 end
--- test/demo_test.exs
+++ test/demo_test.exs
@@ -1,4 +1,15 @@
 defmodule DemoTest do
   use ExUnit.Case
   doctest Demo
+
+  describe "Demo" do
+    test "Executes some lua code, exposing avariable and allowing it to call back to an Elixir function" do
+      script = """
+      return "Hello, " .. name .. "! I’m " .. demo.my_name() .. "."
+      """
+
+      {[result], _state} = Demo.greet(script, "Kerry")
+      assert result == "Hello, Kerry! I’m an Elixir Lua demo."
+    end
+  end
 end
