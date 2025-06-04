da2b3fc0c28017a5b398e18d75c122311f715c5a
Kerry Buckley <kerryjbuckley@gmail.com>
2025-06-04 10:17:43 +0100

    Set up project

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule Demo.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_lua_demo,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:lua, "~> 0.2"}
+    ]
+  end
+end
